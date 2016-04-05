d1946f94feee9e7c4722f235306c76e09c820e7b
Andrew Magill <AndyMagill@Gmail.com>
2016-04-04 23:40:30 -0400

    =Added content, fixed readability and contact form

--- js/main.js
+++ js/main.js
@@ -4,21 +4,44 @@
 */
 
 var ge = {}
-ge.spotlight_anim_speed = 600;
+ge.spotlight_anim_speed = 500;
 
 $(document).ready(function() {
 
 	$('.portfolio_item a').click(function() {
 
-		$('.spotlight_item').slideUp(ge.spotlight_anim_speed);
+		// get the indexes of the elements we want to animate
+		var portfolio_index = $(this).parent('.portfolio_item').index();
+		var visible_index = $('.spotlight_item:visible').index();
 
-		// get the spotlight element we want to animate, current height and target height
-		var spotlight_elem = '.spotlight_item:eq('+$(this).parent('.portfolio_item').index()+')';
-		var target_height = $(spotlight_elem).attr('data-height');
+		// toggle the clicked item as 'selected'
+		$(this).parent('.portfolio_item').toggleClass('selected');
 
-		//animate the element
-		$(spotlight_elem).slideToggle(ge.spotlight_anim_speed);
+		// hide the visible element
+		$('.spotlight_item').slideUp(ge.spotlight_anim_speed, function() {
+			$('.portfolio_item:eq('+visible_index+')').removeClass('selected');
+			console.log($(this).attr('class'));
+		});
+
+		// If the element we want to animate is not the one we just hid,
+		if (visible_index != portfolio_index) {
+
+			// animate the element
+			var spotlight_elem = '.spotlight_item:eq('+portfolio_index+')';
+			$(spotlight_elem).slideToggle(ge.spotlight_anim_speed, function() {
+				console.log($(this).attr('class'));
+			});
+		}
 
 		return false;
 	});
+
+	$("#contact_form").validate({
+		email: {
+			required: true,
+			email: true
+		},
+		body: "required"
+	});
+
 });
